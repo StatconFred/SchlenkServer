0ab45324e4fc16acd12f14cbe72a61b33f516690
Statcon <StatconFred>
2023-10-19 11:55:38 +0000

    executed backup script

---
+++ GitAddCommitPush.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/statcon
+git add . -A
+git commit -m "executed backup script"
+git push https://StatconFred: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@github.com/StatconFred/SchlenkServer.git --all
